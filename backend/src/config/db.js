c67acecd941e9c4c36b40283b0e6ee10be187394
skyraa55 <ekambaramgowthami759@gmail.com>
2026-07-17 19:02:42 +0530

    chnaged the db.js code

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,43 @@
+// import mongoose from "mongoose";
+
+// export async function connectDB() {
+//   const uri = process.env.MONGO_URI;
+
+//   if (!uri) {
+//     throw new Error("MONGO_URI is not set. Check your .env file.");
+//   }
+
+//   mongoose.set("strictQuery", true);
+
+//   await mongoose.connect(uri);
+
+//   console.log(`MongoDB connected -> ${mongoose.connection.name}`);
+// }
+
+
+
+
+
+
+// testing
+
 import mongoose from "mongoose";
 
 export async function connectDB() {
-  const uri = process.env.MONGO_URI;
+  try {
+    const uri = process.env.MONGO_URI;
 
-  if (!uri) {
-    throw new Error("MONGO_URI is not set. Check your .env file.");
+    console.log("Connecting to MongoDB...");
+    console.log("URI Loaded:", !!uri);
+
+    await mongoose.connect(uri);
+
+    console.log("MongoDB connected:", mongoose.connection.name);
+  } catch (err) {
+    console.error("FULL ERROR:");
+    console.error(err);
+    console.error("STACK:");
+    console.error(err.stack);
+    process.exit(1);
   }
-
-  mongoose.set("strictQuery", true);
-
-  await mongoose.connect(uri);
-
-  console.log(`MongoDB connected -> ${mongoose.connection.name}`);
 }
